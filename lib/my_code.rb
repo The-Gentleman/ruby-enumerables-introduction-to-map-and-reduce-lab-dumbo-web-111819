45d7d1a9f110de01a4dc8c04b3fd8f9021fd3c80
Zeshan Raja <zraja4402@gmail.com>
2019-11-14 22:39:59 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,55 @@
-# My Code here....
+def map_to_negativize(source_array)
+results = []
+
+source_array.each do |n|
+results.push(-n)
+
+end 
+results
+end 
+
+def map_to_no_change(source_array)
+source_array
+end 
+
+def map_to_double(source_array)
+results = []
+
+source_array.each do |n| 
+results.push(n * 2)
+
+end 
+results 
+end 
+
+
+def map_to_square(source_array)
+results = []  
+  
+source_array.each do |n|  
+results.push(n * n)
+  
+end 
+results
+end 
+
+def reduce_to_total(source_array, starting_point = 0)
+
+source_array.inject {|total, n| total + n} + starting_point
+
+end 
+
+def reduce_to_all_true(source_array)
+
+source_array.each do |n|
+
+
+end 
+end 
+
+
+def reduce_to_any_true(source_array)
+
+
+end 
+
