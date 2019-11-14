e348c2a42c9dd8d715a1312d45a67a7a8be12d56
Zeshan Raja <zraja4402@gmail.com>
2019-11-14 22:54:15 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -43,15 +43,12 @@ def reduce_to_all_true(source_array)
 counter = 0 
 
 while counter < source_array.size do 
-
 if source_array[counter] = false  
-return false 
+ return false 
 counter += 1 
-
 end 
 return true 
-end 
-end 
+end
 
 def reduce_to_any_true(source_array)
 
